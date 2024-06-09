8c31520e52f7b4ff81a306948ebb5f826c29d7a8
Thalys Márcio <thalys@live.com>
2024-06-08 17:04:16 -0700

    Request Arguments

---
+++ app/controllers/HelloController2.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Controllers;
+
+use AuroraLumina\View\View;
+use App\Services\HelloService;
+use AuroraLumina\Request\ServerRequest;
+use AuroraLumina\Http\Response\Response;
+use AuroraLumina\Request\RequestArguments;
+use AuroraLumina\Controller\BaseController;
+
+class HelloController2 extends BaseController
+{
+    private View $view;
+
+    public function __construct(View $view)
+    {
+        $this->view = $view;
+    }
+
+    public function index(ServerRequest $request, RequestArguments $args, HelloService $helloService): Response
+    {
+        return self::response($this->view->render("hello.lumina", [
+                'name' => $args->name,
+                'hourMinute' => $helloService->hourMinute()
+            ]
+        ));
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,5 +1,10 @@
 <?php
 
+use AuroraLumina\View\View;
+use App\Controllers\HelloController;
+use App\Controllers\HelloController2;
+use AuroraLumina\View\ViewConfiguration;
+
 require __DIR__  . '/../vendor/autoload.php';
 
 (require_once  __DIR__ . '/../app/bootstrap.php')->run(false);
